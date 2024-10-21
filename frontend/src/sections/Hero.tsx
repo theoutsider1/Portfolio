818b2563367da883ed4e7627fc4caf1652bf7419
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-21 17:01:27 +0100

    Create Hero components

---
+++ frontend/src/sections/Hero.tsx
@@ -0,0 +1,46 @@
+import wand from "../../public/assets/wand.png"
+
+export const Hero = ()=>{
+    return (
+        <section className="my-10 p-12 h-4/5 w-full bg-secondary flex flex-col justify-center items-center rounded-3xl">
+           
+            <div> 
+                <h1 className="py-8 text-7xl sm:text-9xl text-primary font-black">
+                    Web Wizard
+                </h1>
+            </div>
+            <div className="w-full flex flex-row  px-4 sm:p-8">
+
+                <div className="w-1/2 flex flex-col ">
+
+                    {/* paragraph */}
+                    <div className="w-3/4">
+                        <p className="text-left text-base sm:text-2xl sm:font-light">Casting spells with code to create magical digital experiences</p>
+                        
+                    </div>
+                    
+
+                    {/* icons */}
+                    <div>
+                        icon 
+                        icon 
+                        icon 
+                    </div>
+                    
+                    {/* Button */}
+                    <div className="my-4 ">
+                        <button className="bg-primary rounded-full py-2.5 px-8 my-4 text-secondary">Contact me</button>
+                    </div>
+                    
+                </div>
+                <div className="w-1/2 flex justify-start relative sm:-top-48">
+                    <img src={wand} className="absolute" alt="wand" />
+                    </div>
+            </div>
+            
+           
+           
+        </section>
+        
+    )
+}
