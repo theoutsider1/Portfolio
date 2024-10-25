bbf9cdb9e46a14cdb055345a6a82fe8494e7d795
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-25 02:45:40 +0100

    Update Responsive style in About section

--- frontend/src/sections/About.tsx
+++ frontend/src/sections/About.tsx
@@ -1,17 +1,17 @@
 export const About = ()=>{
 
     return (
-        <section className="w-full flex sm:flex-row sm:px-12 my-12">
+        <section className="w-full flex sm:flex-row px-4 sm:px-12 my-12">
             {/* Section Title */}
             <div className="w-1/2">
-                <h2 className="text-right items-center text-secondary text-6xl font-bold sm:px-12">About</h2>
+                <h2 className="text-right items-center text-secondary text-4xl sm:text-6xl font-bold sm:px-12">About</h2>
             </div>
             
             {/* Button Section & Description */}
             <div className="w-1/2">
-                <div className="w-full flex justify-end items-center py-4 sm:py-0">
+                <div className="w-full flex justify-end items-center sm:py-0">
                     <button className="flex items-center font-thin text-secondary">
-                        <span className="border-2 text-base font-thin sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-6 sm:px-8">
+                        <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
                             Learn More
                         </span>
 
