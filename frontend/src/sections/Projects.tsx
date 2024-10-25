d682d2d202a3cf9e98e64df7d8639acf2b5daf06
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-25 03:20:24 +0100

    Update responsive style in Projects section

--- frontend/src/sections/Projects.tsx
+++ frontend/src/sections/Projects.tsx
@@ -4,25 +4,26 @@ export const Projects = ()=> {
             {/* Paragraph section */}
             <div className="w-1/2">
                     <div className="w-full h-full flex flex-col justify-end text-secondary ">
-                        <p className=" mb-0 text-sm sm:text-lg font-thin sm:leading-loose sm:px-12 text-right">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
+                        <p className=" sm:mb-0 pt-8 sm:pt-0 text-sm sm:text-base font-thin sm:leading-loose sm:px-12 text-right">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
                              your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
                     </div>
             </div>
 
             {/* Tittle & Button section */}
             <div className="w-1/2">
-                <div className="flex flex-col gap-36">
+                <div className="flex flex-col gap-y-64 sm:gap-36 px-4 sm:px-0">
                     <div className="">
-                        <h2 className="text-right items-center text-secondary text-6xl font-bold sm:px-12">Projects</h2>
+                        <h2 className="text-right items-center text-secondary text-3xl sm:text-6xl font-bold sm:px-12">Projects</h2>
                     </div>
-                    <div className="w-full flex justify-end px-12">
+                    <div className="w-full flex justify-end items-end sm:px-12">
 
                         <button className="flex items-center font-thin text-secondary">
-                                    <span className="border-2 text-base font-thin sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-6 sm:px-8">
+                                    <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
                                         Learn More
                                     </span>
 
-                                    <span className="border-2 border-secondary rounded-full p-1.5 sm:p-2.5 text-center inline-flex items-center">
+                                    {/* Hidden svg, when if the screen is small, to get more space */}
+                                    <span className="hidden border-2 border-secondary rounded-full p-1.5 sm:p-2.5 text-center sm:inline-flex items-center">
                                         <svg
                                         className="w-5 h-5 transform -rotate-45"
                                         aria-hidden="true"
