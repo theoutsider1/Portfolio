8080fccb14d269178f1a7a1bb4aad2bff5f95172
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-25 16:31:10 +0100

    Fix Text Style

--- frontend/src/sections/ProjectsList.tsx
+++ frontend/src/sections/ProjectsList.tsx
@@ -6,12 +6,12 @@ export function ProjectsList() {
             {/* Section contains Title Projects & Description */}
             <div className="w-full border-y-2 border-secondary ">
                 <div className="flex flex-col sm:flex-row gap-4 py-4">
-                    <div className="sm:w-1/4 text-sm sm:text-base text-secondary font-thin sm:font-light">
+                    <div className="sm:w-1/4 text-sm sm:text-base text-secondary font-thin">
                         2024
                     </div>
-                    <div className="sm:w-2/4 sm:flex-row-2 text-sm sm:text-2xl text-secondary font-normal sm:font-bold">
-                         <h4>2jobs</h4>
-                        <p className="mt-2 text-sm sm:text-base">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
+                    <div className="sm:w-2/4  text-secondary ">
+                         <h4 className="text-xl font-semibold sm:text-3xl sm:font-bold">2jobs</h4>
+                        <p className="mt-2 text-sm font-thin sm:text-base">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
                              your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life.</p>
                     </div>
                     <div className="sm:w-1/4 flex justify-end sm:justify-start sm:static">
