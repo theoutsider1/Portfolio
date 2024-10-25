475a89643cb1c40d73d507ace4c938331f91adb9
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-25 15:36:07 +0100

    Update ProjectsList

--- frontend/src/sections/ProjectsList.tsx
+++ frontend/src/sections/ProjectsList.tsx
@@ -1,5 +1,27 @@
-const projectsList = ()=>{
+export function ProjectsList() {
     return (
-        <div></div>
+        <section className="w-full sm:flex flex-col justify-center px-4 sm:px-12 my-12">
+            {/* Divider */}
+             {/* <hr className="px-96 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-secondary to-transparent opacity-75" /> */}
+            {/* Section contains Title Projects & Description */}
+            <div className="w-full border-y-2 border-secondary ">
+                <div className="flex flex-col sm:flex-row gap-4 py-4">
+                    <div className="sm:w-1/4 text-sm sm:text-base text-secondary font-thin sm:font-light">
+                        2024
+                    </div>
+                    <div className="sm:w-2/4 sm:flex-row-2 text-sm sm:text-2xl text-secondary font-normal sm:font-bold">
+                         <h4>2jobs</h4>
+                        <p className="mt-2 text-sm sm:text-base">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
+                             your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life.</p>
+                    </div>
+                    <div className="sm:w-1/4 flex justify-end sm:justify-start sm:static">
+                        <button className="border-2">button</button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Section contains project description  */}
+            
+        </section>
     )
 }
