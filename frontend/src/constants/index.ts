732df8b35284a10e103daf4fd0d249e68d44f238
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-29 15:12:44 +0100

    Create ProjectsInfo List

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,4 @@
-import { NavLinks } from "./Globals/types";
+import { NavLinks, ProjectsInfos } from "./Globals/types";
 
 export const navLinks: NavLinks[] = [
     {
@@ -20,5 +20,31 @@ export const navLinks: NavLinks[] = [
       id: 4,
       name: 'Contact',
       href: '#contact',
+    },
+  ];
+export const ProjectsInfo: ProjectsInfos[] = [
+    {
+      id: 1,
+      name: '2jobs',
+      description: 'Welcome to my magical realm of web development! I’m Hatim Tagmi, your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life.',
+      date: '2024',
+    },
+    {
+      id: 2,
+      name: 'About',
+      description: 'Welcome to my magical realm of web development! I’m Hatim Tagmi, your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life.',
+      date: '2024',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      description: 'Welcome to my magical realm of web development! I’m Hatim Tagmi, your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life.',
+      date: '2024',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      description: 'Welcome to my magical realm of web development! I’m Hatim Tagmi, your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life.',
+      date: '2024',
     },
   ];
