f3262b364c0f4cf338cf9786c8fbfa772328b177
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-29 15:23:15 +0100

    Refactor ProjectsList Component

--- frontend/src/sections/ProjectsList.tsx
+++ frontend/src/sections/ProjectsList.tsx
@@ -4,34 +4,38 @@ import { ProjectsInfo } from "../constants"
 export function ProjectsList() {
     return (
         <section className="w-full sm:flex flex-col justify-center px-4 sm:px-12 my-12">
-
-            
-            {/* Divider */}
-             <hr className="hidden sm:block px-96 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-secondary to-transparent opacity-75" />
-            {/* Section contains Title Projects & Description */}
-            <div className="w-full border-y-2 border-secondary sm:border-none ">
-                {ProjectsInfo.map(project =>(
-                    <div key={project.id} className="flex flex-col sm:flex-row gap-4 py-4">
-                        <div className="sm:w-1/4 text-sm sm:text-base text-secondary font-thin">
-                            {project.date}
+           
+            {ProjectsInfo.map(project => (
+                <div>
+                        {/* Divider */}
+                    <hr className="hidden sm:block px-96 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-secondary to-transparent opacity-75" />
+                    {/* Section contains Title Projects & Description */}
+                    <div className="w-full border-y-2 border-secondary sm:border-none ">
+                        
+                        
+                            <div key={project.id} className="flex flex-col sm:flex-row gap-4 py-4">
+                                <div className="sm:w-1/4 text-sm sm:text-base text-secondary font-thin">
+                                    {project.date}
+                                </div>
+                                <div className="sm:w-2/4  text-secondary ">
+                                    <h4 className="text-xl font-semibold sm:text-3xl sm:font-bold">{project.name}</h4>
+                                    <p className="mt-2 text-sm font-thin sm:text-base">
+                                        {project.description}
+                                    </p>
+                                </div>
+                                <div className="sm:w-1/4 flex justify-end sm:static">
+                                    <button className="flex items-center font-thin text-secondary">
+                                        <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
+                                            Learn More
+                                        </span>     
+                                    </button>
+                                </div>
                         </div>
-                        <div className="sm:w-2/4  text-secondary ">
-                            <h4 className="text-xl font-semibold sm:text-3xl sm:font-bold">{project.name}</h4>
-                            <p className="mt-2 text-sm font-thin sm:text-base">
-                                {project.description}
-                            </p>
-                        </div>
-                        <div className="sm:w-1/4 flex justify-end sm:static">
-                            <button className="flex items-center font-thin text-secondary">
-                                <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
-                                    Learn More
-                                </span>     
-                            </button>
-                        </div>
+                    
+                        
+                    </div>
                 </div>
                 ))}
-                
-            </div>
             {/* Section contains project description  */}
             
         </section>
