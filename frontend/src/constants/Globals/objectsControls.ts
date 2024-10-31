f02e0590534aac5d4eb8f9ff7f31431152133d5a
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-31 16:58:20 +0100

    Add wandControlConfig values

--- frontend/src/constants/Globals/objectsControls.ts
+++ frontend/src/constants/Globals/objectsControls.ts
@@ -4,7 +4,7 @@ interface Control {
     min: number;
     max: number;
 }
-
+// Model Control will be used to controll the model using Leva
 interface ModelControl {
     rotationX: Control;
     rotationY: Control;
@@ -17,3 +17,41 @@ interface ModelControl {
     scale: Control;
 }
 
+// wandControlConfig
+export const wandControlConfig : ModelControl = {
+    rotationX: {
+        value : 0,
+        min: -10,
+        max: 10,
+      },
+      rotationY: {
+        value : -6.6,
+        min: -10,
+        max: 10,
+      },
+      rotationZ: {
+        value : 0.4,
+        min: -10,
+        max: 10,
+      },
+      positionX: {
+        value : 0,
+        min: -10,
+        max: 10,
+      },
+      positionY: {
+        value : -1.9,
+        min: -10,
+        max: 10,
+      },
+      positionZ: {
+        value : -5.5,
+        min: -10,
+        max: 10,
+      },
+      scale : {
+        value: 0.2,
+        min: 0.1,
+        max: 10,
+    }
+}
