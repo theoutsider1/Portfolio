b161c9ba283a61efd920be5960dd17aad3861f06
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-10-31 17:25:29 +0100

    Refactor Stack component using objectsControls

--- frontend/src/sections/Stack.tsx
+++ frontend/src/sections/Stack.tsx
@@ -5,45 +5,12 @@ import MagicWand from "../components/MagicWand";
 import CanvasLoader from "../components/CanvasLoader";
 import { Leva, useControls } from "leva";
 import { useMediaQuery } from "react-responsive";
+import { CrystalBall } from "../components/CrystalBall";
+import { wandControlConfig } from "../constants/Globals/objectsControls";
 
 export const Stack = () => {
-  const controls = useControls({
-    rotationX: {
-      value : 0,
-      min: -10,
-      max: 10,
-    },
-    rotationY: {
-      value : -6.6,
-      min: -10,
-      max: 10,
-    },
-    rotationZ: {
-      value : 0.4,
-      min: -10,
-      max: 10,
-    },
-    positionX: {
-      value : 0,
-      min: -10,
-      max: 10,
-    },
-    positionY: {
-      value : -1.9,
-      min: -10,
-      max: 10,
-    },
-    positionZ: {
-      value : -5.5,
-      min: -10,
-      max: 10,
-    },
-    scale : {
-      value: 0.2,
-      min: 0.1,
-      max: 10,
-    }
-  }) 
+  
+  const wandControl = useControls({...wandControlConfig}) 
   const isMobile = useMediaQuery({maxWidth: 768})
   // const isTablet = useMediaQuery({minWidth:768, maxWidth: 1024})
   return (
@@ -58,10 +25,14 @@ export const Stack = () => {
                     // scale={[0,0,7]}
                     // position={[0,0,0]} 
                     // rotation={[0,280,0]}
-                    position={[controls.positionX, controls.positionY, controls.positionZ]} 
-                    rotation={[controls.rotationX, controls.rotationY, controls.rotationZ]}
-                    scale={isMobile ? 0.13 : controls.scale}                  
+                    position={[wandControl.positionX, wandControl.positionY, wandControl.positionZ]} 
+                    rotation={[wandControl.rotationX, wandControl.rotationY, wandControl.rotationZ]}
+                    scale={isMobile ? 0.13 : wandControl.scale}                  
                     />
+                <CrystalBall
+                    position={[wandControl.positionX, wandControl.positionY, wandControl.positionZ]} 
+                    rotation={[wandControl.rotationX, wandControl.rotationY, wandControl.rotationZ]}
+                    scale={isMobile ? 0.13 : wandControl.scale} />
                 <ambientLight intensity={9}/>
                 <directionalLight position={[10,10,10]}/>
               </Suspense>
