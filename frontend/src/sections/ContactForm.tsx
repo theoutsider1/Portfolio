ce5583e03613d0b889bc11e67dcc266a79408cbb
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-11-25 16:33:01 +0100

    Add serviceId, templateId, PublicKey from env

--- frontend/src/sections/ContactForm.tsx
+++ frontend/src/sections/ContactForm.tsx
@@ -5,14 +5,19 @@ import emailjs from '@emailjs/browser';
 
 export const ContactForm = ()=>{
     const form = useRef<HTMLFormElement>(null);
-
+    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const PublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
         if (form.current){
 
             emailjs
-              .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-                publicKey: 'YOUR_PUBLIC_KEY',
+              .sendForm(
+                serviceId,
+                templateId,
+                 form.current, {
+                publicKey: PublicKey,
               })
               .then(
                 () => {
@@ -34,15 +39,15 @@ export const ContactForm = ()=>{
                 <form ref={form} onSubmit={sendEmail} className="space-y-8">
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-white ">Your email</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@flowbite.com" required/>
+                        <input type="email" name="from_mail" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@flowbite.com" required/>
                     </div>
                     <div>
                         <label htmlFor="subject" className="block mb-2 text-sm font-medium text-white ">Subject</label>
-                        <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required/>
+                        <input type="text" id="subject" name="from_subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required/>
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Your message</label>
-                        <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Leave a comment..."></textarea>
+                        <textarea id="message" rows={6} name="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Leave a comment..."></textarea>
                     </div>
                     <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-[#171720] rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
                 </form>
