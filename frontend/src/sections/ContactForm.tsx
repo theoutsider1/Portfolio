505eedc6770b6e3824a31ca271209aa7c61af934
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-11-25 17:25:11 +0100

    Create stateMessage to show success message

--- frontend/src/sections/ContactForm.tsx
+++ frontend/src/sections/ContactForm.tsx
@@ -1,15 +1,18 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
 
 
 
 export const ContactForm = ()=>{
     const form = useRef<HTMLFormElement>(null);
+    const [stateMessage, setStateMessage] = useState<string>('');
+
     const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
     const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
     const PublicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
+      setStateMessage('Sending Message')
         if (form.current){
 
             emailjs
@@ -21,6 +24,7 @@ export const ContactForm = ()=>{
               })
               .then(
                 () => {
+                    setStateMessage('Message Sent')
                   console.log('SUCCESS!');
                 },
                 (error) => {
@@ -49,7 +53,18 @@ export const ContactForm = ()=>{
                         <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Your message</label>
                         <textarea id="message" rows={6} name="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Leave a comment..."></textarea>
                     </div>
-                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-[#171720] rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
+                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-[#171720] rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300" disabled={stateMessage === 'Sending Message' ? true : false} >Send message</button>
+                    {stateMessage === '' ?
+                        null : 
+                        <div className="mb-2 text-sm font-medium text-green-500">
+                            <p className="inline-flex">
+                                {stateMessage} 
+                                <span className=''>
+                                    <svg aria-hidden="true" className="w-5 h-5 text-green-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                                </span>
+                            </p>
+                        </div>
+                    }
                 </form>
             </div>
         </section>
