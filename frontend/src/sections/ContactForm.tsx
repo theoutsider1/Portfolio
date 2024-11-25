5088bafa9c03d3997c784b0d5b48342d0faa6968
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-11-25 17:51:45 +0100

    Update ContactForm & Refactor it

--- frontend/src/sections/ContactForm.tsx
+++ frontend/src/sections/ContactForm.tsx
@@ -38,22 +38,40 @@ export const ContactForm = ()=>{
     return (
         <section className="bg-[#091122] ">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white ">Contact Us</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-white ">Get In Touch</h2>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Want to send feedback ? Let me know.</p>
+                {/* Contact Form */}
                 <form ref={form} onSubmit={sendEmail} className="space-y-8">
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-white ">Your email</label>
-                        <input type="email" name="from_mail" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@flowbite.com" required/>
+                        <input type="email" 
+                                name="from_mail" 
+                                id="email" 
+                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" 
+                                placeholder="Your Email Adress" required/>
                     </div>
                     <div>
                         <label htmlFor="subject" className="block mb-2 text-sm font-medium text-white ">Subject</label>
-                        <input type="text" id="subject" name="from_subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required/>
+                        <input type="text" 
+                                id="subject" 
+                                name="from_subject" 
+                                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" 
+                                placeholder="Let us know how we can help you" required/>
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Your message</label>
-                        <textarea id="message" rows={6} name="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder="Leave a comment..."></textarea>
+                        <textarea id="message" 
+                                    rows={6} 
+                                    name="message" 
+                                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " 
+                                    placeholder="Leave a comment...">
+                        </textarea>
                     </div>
-                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-[#171720] rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300" disabled={stateMessage === 'Sending Message' ? true : false} >Send message</button>
+                    <button type="submit" 
+                            className="py-3 px-5 text-sm font-medium text-center text-[#171720] rounded-lg bg-white sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300" 
+                            disabled={stateMessage === 'Sending Message' ? true : false} >
+                                Send message
+                    </button>
                     {stateMessage === '' ?
                         null : 
                         <div className="mb-2 text-sm font-medium text-green-500">
