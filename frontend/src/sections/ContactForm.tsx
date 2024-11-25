0de0c4b5671f3b9448ca8bcb222ef4833ffb5202
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-11-25 17:34:24 +0100

    Fix success message style

--- frontend/src/sections/ContactForm.tsx
+++ frontend/src/sections/ContactForm.tsx
@@ -57,9 +57,9 @@ export const ContactForm = ()=>{
                     {stateMessage === '' ?
                         null : 
                         <div className="mb-2 text-sm font-medium text-green-500">
-                            <p className="inline-flex">
+                            <p className={`inline-flex ${stateMessage === 'Sending Message' ? 'text-orange-500' : ''}`}>
                                 {stateMessage} 
-                                <span className=''>
+                                <span className={`${stateMessage === 'Sending Message' ? 'hidden' : ''}`}>
                                     <svg aria-hidden="true" className="w-5 h-5 text-green-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                                 </span>
                             </p>
