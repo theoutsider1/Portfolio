5958b94e48bcec85feed745e47faccb12ec60b73
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-11-30 15:28:08 +0100

    Create PlaneGeometry

--- frontend/src/sections/Stack.tsx
+++ frontend/src/sections/Stack.tsx
@@ -5,14 +5,12 @@ import { Suspense, useRef } from "react";
 import CanvasLoader from "../components/CanvasLoader";
 import { Leva, useControls } from "leva";
 import { useMediaQuery } from "react-responsive";
-// import { CrystalBall } from "../components/CrystalBall";
 import { wandControlConfig } from "../constants/Globals/objectsControls";
-import { StackTitle } from "../components/StackTitle";
 import { IconsMesh } from "../components/IconsMesh";
 import { gsap } from "gsap";
 import { Mesh } from "three";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Environment } from "@react-three/drei";
+import { Environment, MeshReflectorMaterial } from "@react-three/drei";
 
 gsap.registerPlugin(ScrollTrigger)
 export const Stack = () => {
@@ -28,17 +26,33 @@ export const Stack = () => {
   return (
         <section id='three' ref={testRef} className="w-full min-h-screen  relative">
             <Leva/>
+            <h2 className="text-right items-center text-secondary text-3xl sm:text-6xl font-bold sm:px-12">Projects</h2>
             <Canvas shadows dpr={[1, 1.5]} camera={{position: [0,0,5], fov:50}} 
                 style={{
                   width: `100vw`,
                   height: `100vh`
                 }}>
               <color attach="background" args={['#171720']}/>
-
+              <mesh position={-0.48} rotation-x={[-Math.PI /2]}>
+            <planeGeometry  args={[100,120,1,32]}/>
+                <MeshReflectorMaterial 
+                    blur={[600, 200]}
+                    resolution={2048}
+                    mixBlur={1}
+                    mixStrength={80}
+                    roughness={0.8} // Adjusted for a more natural surface
+                    depthScale={1.2}
+                    minDepthThreshold={0.4}
+                    maxDepthThreshold={1.4}
+                    color="#171720" // Harmonized with the background
+                    metalness={0.1} // Lower for a matte appearance
+                    mirror={1}
+                />
+        </mesh>
               <Suspense fallback={<CanvasLoader/>}>
 
                 {/* StackTitle Component */}
-                <StackTitle ref={titleRef}/>
+                {/* <StackTitle ref={titleRef}/> */}
                 
                 {/* MagicWand component */}
                 <MagicWand 
@@ -51,11 +65,10 @@ export const Stack = () => {
 
                 {/* Icons */}
                 
-                    <IconsMesh testRef={testRef}/>
-                
+                  <IconsMesh testRef={testRef}/>
+
                   <Environment preset="sunset"/>
-                  <ambientLight intensity={3}/>
-                  
+                  <ambientLight castShadow intensity={3}/>
                   <directionalLight 
                     castShadow 
                     position={[0,0,5]} 
