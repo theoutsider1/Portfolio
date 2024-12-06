c5e9137f275e56c40fbfc79d8d41957c82c61d28
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-06 16:09:52 +0100

    Update ProjectsInfo

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -24,27 +24,27 @@ export const navLinks: NavLinks[] = [
   ];
 export const ProjectsInfo: ProjectsInfos[] = [
     {
-      id: 1,
+        id: 1,
+        name: 'JosoPress Wordpress Website Customization',
+        description: 'Customized the purchased Josopress WordPress theme for a news website using WordPress and Elementor, adjusting the layout and functionality to meet the client\'s needs.',
+        date: '2024',
+      },
+    { 
+      id: 2,
       name: 'Responsive Booking interface (Stepper Form)',
       description: 'Developed a responsive booking interface featuring a step-by-step form for the Safiir Educational Advice and Study Guidance website. The interface allows users to easily navigate through the booking process. ',
       date: '2024',
     },
     {
-      id: 2,
+      id: 3,
       name: '2Jobs Platform',
       description: 'Developed the 2Jobs platform, which connects job seekers with companies looking for candidates. A key feature of the platform is allowing job seekers to apply for jobs with minimal information, such as just their phone number.',
       date: '2024',
     },
     {
-      id: 3,
+      id: 4,
       name: 'Petify E-commerce Store',
       description: 'Contributed to the development of Petify, an e-commerce store built by a team during my MERN bootcamp as a PFF. The platform features a modern, responsive user interface. ',
       date: '2023',
-    },
-    {
-      id: 4,
-      name: 'JosoPress Wordpress Website Customization',
-      description: 'Customized the purchased Josopress WordPress theme for a news website using WordPress and Elementor, adjusting the layout and functionality to meet the client\'s needs.',
-      date: '2024',
     },
   ];
