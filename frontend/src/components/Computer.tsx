d19a9cb0487019ea5d2834a71bbc4e35b05f607e
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-13 16:16:54 +0100

    Added planeGeometry to fix video rotation

--- frontend/src/components/Computer.tsx
+++ frontend/src/components/Computer.tsx
@@ -46,9 +46,11 @@ export function Compouter(props: JSX.IntrinsicElements['group']) {
           geometry={nodes['monitor-screen'].geometry}
           material={nodes['monitor-screen'].material}
           position={[0.127, 1.831, 0.511]}
-          rotation={[1.571, -0.005, 0.031]}
+          rotation={[0.571, -0.005, 0.031]}
           scale={[0.661, 0.608, 0.401]}
         >
+          <planeGeometry args={[1,1]}/>
+          
           <meshBasicMaterial map={txt}/>
          
         </mesh>
