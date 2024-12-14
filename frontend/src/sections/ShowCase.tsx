e1f06e4423441e17c523520dbc60e5dc910e5029
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-14 16:34:18 +0100

    Replaced ambientlight with pointLight

--- frontend/src/sections/ShowCase.tsx
+++ frontend/src/sections/ShowCase.tsx
@@ -119,7 +119,7 @@ const ShowCase = ()=>{
            
             <div className=" col-span-1 m-4 border-2">
                 <Canvas camera={{ position: [0, 0, -20], fov: 50 }}>
-                        <ambientLight intensity={5}/>
+                    <pointLight position={[10, 10, 10]} intensity={1.5} />
                         <directionalLight position={[0, -5, 5]}/>
                         <Center>
                             <Suspense fallback={<CanvasLoader/>}>
