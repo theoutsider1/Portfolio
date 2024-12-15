2c5a51e7b4fe18315a7de6f3592dce333c3bcbc8
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-15 12:48:35 +0100

    Added Tech interfa & updated ProjectsInfos interfa

--- frontend/src/constants/Globals/types.ts
+++ frontend/src/constants/Globals/types.ts
@@ -3,10 +3,18 @@ export interface NavLinks {
     name: string,
     href : string
 }
+export interface Tech {
+    id: number,
+    name: string,
+    href : string
+}
 
 export interface ProjectsInfos {
     id: number,
     name: string,
     description: string,
     date: string,
+    tech: Tech[],
+    projectDirection: string,
 }
+
