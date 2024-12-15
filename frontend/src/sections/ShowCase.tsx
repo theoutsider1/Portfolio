0ffd17989779cbd425d5750ee0ab74f4de5ad02f
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-15 13:49:44 +0100

    Add dynamic styles for swiperRight

--- frontend/src/sections/ShowCase.tsx
+++ frontend/src/sections/ShowCase.tsx
@@ -95,7 +95,7 @@ const ShowCase = ()=>{
                         </span>
                     </button>
                     <button onClick={()=>swipeProjects('r')} disabled={swiperRight}>
-                        <span className="hidden border-2 border-secondary border-opacity-25 rounded-xl p-1.5 sm:p-2.5 text-center text-gray-400 sm:inline-flex items-center">
+                        <span className={`hidden border-2 border-secondary rounded-xl p-1.5 sm:p-2.5 text-center ${swiperRight === false ? `border-opacity-50 text-gray-100` : ` border-opacity-25 text-gray-400`} sm:inline-flex items-center`}>
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
