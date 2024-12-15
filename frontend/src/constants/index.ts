fcc4592c7bf921ebfc355ec9627b3f0d512ee559
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-15 12:49:20 +0100

    Updated navLinks

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -18,6 +18,11 @@ export const navLinks: NavLinks[] = [
     },
     {
       id: 4,
+      name: 'Stack',
+      href: '#stack',
+    },
+    {
+      id: 5,
       name: 'Contact',
       href: '#contact',
     },
@@ -28,23 +33,151 @@ export const ProjectsInfo: ProjectsInfos[] = [
         name: 'JosoPress Wordpress Website Customization',
         description: 'Customized the purchased Josopress WordPress theme for a news website using WordPress and Elementor, adjusting the layout and functionality to meet the client\'s needs.',
         date: '2024',
+        tech: [{
+          id:10,
+          name: 'dsada',
+          href: 'ddada'
+        }],
+        projectDirection: '/textures/project/petify.mp4',
       },
     { 
       id: 2,
       name: 'Responsive Booking interface (Stepper Form)',
       description: 'Developed a responsive booking interface featuring a step-by-step form for the Safiir Educational Advice and Study Guidance website. The interface allows users to easily navigate through the booking process. ',
       date: '2024',
+      tech: [
+        {
+        id:10,
+        name: 'React',
+        href: 'assets/react.svg'
+        },
+        {
+        id:11,
+        name: 'PrimeReact',
+        href: 'assets/react.svg'
+        },
+        {
+          id:12,
+          name: 'Tailwind',
+          href: 'assets/tailwindcss.svg'
+        },
+        {
+          id:13,
+          name: 'Javascript',
+          href: 'assets/javascript.svg'
+        },
+        {
+          id:14,
+          name: 'I18n Translator',
+          href: 'assets/javascript.svg'
+        }],
+        projectDirection: '/textures/project/petify.mp4',
     },
     {
       id: 3,
       name: '2Jobs Platform',
       description: 'Developed the 2Jobs platform, which connects job seekers with companies looking for candidates. A key feature of the platform is allowing job seekers to apply for jobs with minimal information, such as just their phone number.',
       date: '2024',
+      tech: [
+        {
+        id:10,
+        name: 'React',
+        href: 'assets/react.svg'
+        },
+        {
+        id:11,
+        name: 'Tailwind',
+        href: 'assets/tailwindcss.svg'
+        },
+        {
+          id:12,
+          name: 'Axios',
+          href: 'assets/notion.svg'
+        },
+        {
+          id:13,
+          name: 'Typescript',
+          href: 'assets/typescript.svg'
+        },
+        {
+          id:14,
+          name: 'nest',
+          href: 'assets/nest.svg'
+        },
+        {
+          id:15,
+          name: 'node js',
+          href: 'assets/nodejs.svg'
+        },
+        {
+          id:16,
+          name: 'Postman',
+          href: 'assets/postman.svg'
+        },
+        {
+          id:17,
+          name: 'PostgreSQL',
+          href: 'assets/postgresql.svg'
+        },
+        {
+          id:18,
+          name: 'github',
+          href: 'assets/github.svg'
+        },
+        {
+          id:19,
+          name: 'firgma',
+          href: 'assets/figma.svg'
+        },
+      ],
+      projectDirection: '/textures/project/petify.mp4',
     },
     {
       id: 4,
       name: 'Petify E-commerce Store',
       description: 'Contributed to the development of Petify, an e-commerce store built by a team during my MERN bootcamp as a PFF. The platform features a modern, responsive user interface. ',
       date: '2023',
+      tech: [
+        {
+          id:10,
+          name: 'React',
+          href: 'assets/react.svg'
+          },
+          {
+          id:11,
+          name: 'Tailwind',
+          href: 'assets/tailwindcss.svg'
+          },
+          {
+            id:12,
+            name: 'Axios',
+            href: 'assets/notion.svg'
+          },
+          {
+            id:13,
+            name: 'Typescript',
+            href: 'assets/typescript.svg'
+          },
+          {
+            id:14,
+            name: 'nest',
+            href: 'assets/nest.svg'
+          },
+          {
+          id:15,
+          name: 'node js',
+          href: 'assets/nodejs.svg'
+          },
+          {
+            id:16,
+            name: 'Postman',
+            href: 'assets/postman.svg'
+          },
+          {
+            id:17,
+            name: 'PostgreSQL',
+            href: 'assets/postgresql.svg'
+        },],
+        projectDirection: '/textures/project/petify.mp4',
     },
   ];
