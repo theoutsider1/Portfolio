3759edf194ec740e424c91b874bbd59ab1eabb81
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-18 12:22:19 +0100

    destructuried Props

--- frontend/src/sections/About.tsx
+++ frontend/src/sections/About.tsx
@@ -1,4 +1,5 @@
-export const About = ()=>{
+
+export const About = (isMobile)=>{
 
     return (
         <section id="about" className="w-full flex flex-col sm:flex-row px-4 sm:px-12 sm:my-12">
@@ -8,39 +9,42 @@ export const About = ()=>{
             </div>
             
             {/* Button Section & Description */}
-            <div className="w-1/2">
-                <div className="w-full flex justify-end items-center sm:py-0">
-                    <button className="flex items-center font-thin text-secondary">
-                        <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
-                            Learn More
-                        </span>
+           
+           
+            <div className="sm:w-1/2">
+             <div className="w-full flex justify-end items-center sm:py-0">
+             <button className="flex items-center font-thin text-secondary">
+                 <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
+                     Learn More
+                 </span>
 
-                        <span className="border-2 border-secondary rounded-full p-1.5 sm:p-2.5 text-center inline-flex items-center">
-                            <svg
-                            className="w-5 h-5 transform -rotate-45"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 14 10"
-                            >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="0.5"
-                                d="M1 5h12m0 0L9 1m4 4L9 9"
-                            />
-                            </svg>
-                            <span className="sr-only">Icon description</span>
-                        </span>
-                    </button>
-                   
-                </div>
-                <div className="w-full flex justify-start my-4 py-12 text-secondary ">
-                    <p className="text-sm sm:text-lg font-thin sm:leading-loose">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
-                        your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
-                </div>
+                 <span className="border-2 border-secondary rounded-full p-1.5 sm:p-2.5 text-center inline-flex items-center">
+                     <svg
+                     className="w-5 h-5 transform -rotate-45"
+                     aria-hidden="true"
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 14 10"
+                     >
+                     <path
+                         stroke="currentColor"
+                         strokeLinecap="round"
+                         strokeLinejoin="round"
+                         strokeWidth="0.5"
+                         d="M1 5h12m0 0L9 1m4 4L9 9"
+                     />
+                     </svg>
+                     <span className="sr-only">Icon description</span>
+                 </span>
+             </button>
+            
+         </div>
+         <div className="w-full flex justify-start my-4 py-12 text-secondary ">
+             <p className="text-sm sm:text-lg font-thin sm:leading-loose">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
+                 your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
+         </div>
 
+               
                 
             </div>
         </section>
