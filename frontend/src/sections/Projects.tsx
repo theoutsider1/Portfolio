69b251b9e7accbb00a0c67115d477e9ae0b95c7a
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-19 09:56:50 +0100

    Changed button with Btn

--- frontend/src/sections/Projects.tsx
+++ frontend/src/sections/Projects.tsx
@@ -12,7 +12,9 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
             {/* Paragraph section */}
             <div className="w-1/2">
                     <div className="w-full h-full flex flex-col justify-end items-end text-secondary ">
-                  
+                   <Btn
+                        btnText={projectSectionContent.btnText}
+                        isMobile={isMobile}/>
 
                         <p className=" sm:mb-0 pt-8 sm:pt-0 text-sm sm:text-base font-thin sm:leading-loose sm:px-12 text-right">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
                              your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
