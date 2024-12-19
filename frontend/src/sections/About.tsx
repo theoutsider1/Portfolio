b6cc99bb6426218c61899bc630a988dd47dc649f
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-19 03:23:07 +0100

    Refactored About and Created aboutContent

--- frontend/src/sections/About.tsx
+++ frontend/src/sections/About.tsx
@@ -1,56 +1,53 @@
 import React from "react";
+import { Btn } from "../components/Btn";
+import { SectionContent } from "../constants";
+import { SectionDetails } from "../constants/Globals/types";
 
 interface AboutProps {
     isMobile : boolean;
 }
-export const About: React.FC<AboutProps> = (isMobile)=>{
-
+export const About: React.FC<AboutProps> = ({isMobile})=>{
+    const aboutContnet: SectionDetails = SectionContent.About
     return (
         <section id="about" className="w-full flex flex-col sm:flex-row px-4 sm:px-12 sm:my-12">
             {/* Section Title */}
             <div className="sm:w-1/2">
-                <h2 className="text-left sm:text-right items-center text-secondary text-4xl sm:text-6xl font-bold sm:px-12">About</h2>
+                <h2 className="text-left sm:text-right items-center text-secondary text-4xl sm:text-6xl font-bold sm:px-12">
+                    About
+                </h2>
             </div>
             
             {/* Button Section & Description */}
            
-           
+           { 
+           !isMobile ? (
             <div className="sm:w-1/2">
-             <div className="w-full flex justify-end items-center sm:py-0">
-             <button className="flex items-center font-thin text-secondary">
-                 <span className="border-2 text-sm sm:text-base sm:font-light inline-flex items-center sm:my-4 border-secondary rounded-full py-1.5 sm:py-2.5 px-4 sm:px-8">
-                     Learn More
-                 </span>
+                <div className="w-full flex sm:justify-end items-center">
+                
+                    {/* <Btn/> */}
 
-                 <span className="border-2 border-secondary rounded-full p-1.5 sm:p-2.5 text-center inline-flex items-center">
-                     <svg
-                     className="w-5 h-5 transform -rotate-45"
-                     aria-hidden="true"
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 14 10"
-                     >
-                     <path
-                         stroke="currentColor"
-                         strokeLinecap="round"
-                         strokeLinejoin="round"
-                         strokeWidth="0.5"
-                         d="M1 5h12m0 0L9 1m4 4L9 9"
-                     />
-                     </svg>
-                     <span className="sr-only">Icon description</span>
-                 </span>
-             </button>
-            
-         </div>
-         <div className="w-full flex justify-start my-4 py-12 text-secondary ">
-             <p className="text-sm sm:text-lg font-thin sm:leading-loose">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
-                 your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
-         </div>
+                </div>
+                <div className="w-full my-4 py-12 text-secondary ">
+                    <p className="text-sm sm:text-lg font-thin sm:leading-loose">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
+                        your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
+                </div>
 
                
                 
+            </div>)
+            : 
+            <div className="sm:w-1/2">
+                <div className="w-full flex justify-start my-4 py-12 text-secondary ">
+                
+                <p className="text-sm sm:text-lg font-thin sm:leading-loose">Welcome to my magical realm of web development! I’m Hatim Tagmi, 
+                    your friendly neighborhood Web Wizard, casting spells of creativity and innovation to bring your digital dreams to life. Armed with my trusty wand (or keyboard), I conjure captivating websites that blend enchanting designs with powerful functionality.</p>
+                
+                </div>
+                <div className= "w-full flex justify-end items-center sm:py-0" >
+                {/* <Btn/> */}
+                </div>   
             </div>
+            }
         </section>
     )
 }
