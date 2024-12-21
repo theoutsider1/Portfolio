e016ed5b0963cfd8e3696c0cef61e0d47942e841
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-21 14:56:56 +0100

    Optimized Projects component style

--- frontend/src/sections/Projects.tsx
+++ frontend/src/sections/Projects.tsx
@@ -8,59 +8,52 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
     
     const projectSectionContent = SectionContent.Project
     return (
-        <section id="work" className="flex">
+        <section id="work" className="flex my-4 sm:my-0">
             {/* Paragraph section */}
             {!isMobile ? 
             <>
-            <div className="sm:w-1/2">
+            <div className="sm:w-1/2 ">
                     <div className="w-full h-full flex flex-col justify-end items-end text-secondary ">
                    <Btn
                         btnText={projectSectionContent.btnText}
                         isMobile={isMobile}
                     />
 
-                        <p className="w-full sm:mb-0 pt-8 sm:pt-0 text-sm sm:text-base font-thin sm:leading-loose sm:px-12 text-right">
+                        <p className="w-full sm:mb-0 my-8 text-base font-thin leading-loose px-12 text-right">
                             {projectSectionContent.paragraph}
                         </p>
                     </div>
             </div>
 
             {/* Tittle & Button section */}
-            <div className="sm:w-1/2">
-                <div className="flex flex-col  px-4 sm:px-0">
+                <div className="w-1/2 flex flex-col px-4 sm:px-0">
                     <div className="">
-                        <h2 className="text-right items-center text-secondary text-3xl sm:text-6xl font-bold sm:px-12">{projectSectionContent.title}</h2>
+                        <h2 className="text-right items-center text-secondary text-6xl font-bold px-12">{projectSectionContent.title}</h2>
                     </div>
                    
                 </div>
-                
-            </div>
             </>
             : 
-            <div className="flex flex-col">
+            <div className="flex flex-col px-4">
             {/* Tittle & Button section */}
-            <div className="sm:w-1/2">
-                <div className="flex flex-col  px-4 sm:px-0">
-                    <div className="">
-                        <h2 className="text-right items-center text-secondary text-3xl sm:text-6xl font-bold sm:px-12">{projectSectionContent.title}</h2>
-                    </div>
-                   
-                </div>
-                
-            </div>
             <div className="">
+                        <h2 className="text-left items-center text-secondary text-3xl font-bold">{projectSectionContent.title}</h2>
+            </div>
+            
                     <div className="w-full h-full flex flex-col justify-end items-end text-secondary ">
-                        <p className="w-full sm:mb-0 pt-8 sm:pt-0 text-sm sm:text-base font-thin sm:leading-loose sm:px-12 text-right">
+                        <p className="w-full pt-8 text-sm font-thin text-left">
                             {projectSectionContent.paragraph}
                         </p>
                     </div>
-            </div>
+                    <div className="w-full flex justify-end items-center my-2">
+                        <Btn
+                                    btnText={projectSectionContent.btnText}
+                                    isMobile={isMobile}
+                                />
+                    </div>
+         
 
             
-            <Btn
-                        btnText={projectSectionContent.btnText}
-                        isMobile={isMobile}
-                    />
             </div>
             }
         </section>
