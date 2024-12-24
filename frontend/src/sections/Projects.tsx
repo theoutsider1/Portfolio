820c5672c6d97ab8da25d5f70c3e1fc652e3688c
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-24 15:45:00 +0100

    feat: wrap Btn component with a div using className sm:px-12

--- frontend/src/sections/Projects.tsx
+++ frontend/src/sections/Projects.tsx
@@ -14,11 +14,13 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
             <>
             <div className="sm:w-1/2 ">
                     <div className="w-full h-full flex flex-col justify-end items-end text-secondary ">
-                   <Btn
-                        btnText={projectSectionContent.btnText}
-                        isMobile={isMobile}
-                    />
-
+                    <div className="sm:px-12">
+                        <Btn
+                                btnText={projectSectionContent.btnText}
+                                isMobile={isMobile}
+                                
+                            />
+                    </div>
                         <p className="w-full sm:mb-0 my-8 text-base font-thin leading-loose px-12 text-right">
                             {projectSectionContent.paragraph}
                         </p>
