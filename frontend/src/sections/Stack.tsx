3e784e4efa79908c4e9e297a865c6a561582d264
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-28 17:47:15 +0100

    feat: add toggle button to switch between magical and muggle stack descriptions

--- frontend/src/sections/Stack.tsx
+++ frontend/src/sections/Stack.tsx
@@ -47,22 +47,28 @@ const HumanLangue = ()=>{
                 <img src='/assets/spotlight5.png' className="absolute transform  -scale-x-100 -left-4 opacity-70 rounded-3xl pointer-events-none" alt="" />
                 <h2 className="pt-4 sm:text-right items-center text-secondary text-4xl sm:text-6xl font-bold ">Stack</h2>
                 <div className=" w-full sm:text-right sm:my-8  text-secondary ">
-                {!showMuggleVersion ? (
-            <>
-              <p className="text-sm sm:text-base font-thin sm:leading-loose">
-                In my spells room, I weave magic using HTMLus Arcana, CSSus Shieldum,
-                Tailwindus Bractum, JavaScriptus Fluxia, Typescriptus Magica, Reacto
-                Interfacia, Reduxus Imperium, Axiosum Revelia, Prime Reactum Lumina,
-                GSAPus Tempus, React Fiberum Revelia, Nestus Fortificus, Expressium
-                Maxima, Rest APIus Revelia, MongoDBus Mysticius, PostgreSQLum Datafluxia,
-                Prismaum Arcana, Supabaseum Revelia, and WordPresso Craftia to weave
-                modern digital enchantments.
-              </p>
-            </>
-          ) : (
-            <HumanLangue />
-          )}
-          <div>
+                  {!showMuggleVersion ? (
+                    <>
+                      <p className="text-sm sm:text-base font-thin sm:leading-loose">
+                        In my spells room, I weave magic using HTMLus Arcana, CSSus Shieldum,
+                        Tailwindus Bractum, JavaScriptus Fluxia, Typescriptus Magica, Reacto
+                        Interfacia, Reduxus Imperium, Axiosum Revelia, Prime Reactum Lumina,
+                        GSAPus Tempus, React Fiberum Revelia, Nestus Fortificus, Expressium
+                        Maxima, Rest APIus Revelia, MongoDBus Mysticius, PostgreSQLum Datafluxia,
+                        Prismaum Arcana, Supabaseum Revelia, and WordPresso Craftia to weave
+                        modern digital enchantments.
+                      </p>
+                    </>
+                  ) : (
+                    <HumanLangue />
+                  )}
+                <div>
+                <span
+                  className="text-xs text-slate-200 underline cursor-pointer z-50"
+                  onClick={() => setShowMuggleVersion(!showMuggleVersion)}>
+                    {showMuggleVersion ? "Magical version!" : "Muggle version!"}
+                </span>
+
           </div>
                 </div>
             </div>
