1cd6659c61f8cf63fdfe06b59fc66648aabccfcc
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-12-30 02:28:51 +0100

    feat: add state management for top button visibility

--- frontend/src/components/ScrollTopBtn.tsx
+++ frontend/src/components/ScrollTopBtn.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 
 export const ScrollTopBtn = ()=>{
 
-  
+    const [showTopBtn, setShowTopBtn]= useState(false)
+   
     return (
         <div className={`relative z-20 cursor-pointer` }>
-             
+            {showTopBtn && 
                <span className="bg-white-0 backdrop-filter backdrop-blur-sm bg-opacity-20 fixed right-28 bottom-10 text-white border-2 rounded-xl text-center sm:inline-flex items-center border-secondary p-1.5 sm:p-2.5" onClick={handleScrollTop} >
                     <svg
                         className="w-5 h-5 transform -rotate-90"
@@ -24,7 +25,7 @@ export const ScrollTopBtn = ()=>{
                     </svg>
                     <span className="sr-only">Icon description</span>
                </span>
-             
+               }
         </div>
     )
 }
