ea589d76bb718bddf058a2cfd069fb33b6ca811c
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-04 12:26:10 +0100

    add new fonts and font weights in taiwlind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,9 +8,26 @@ export default {
     extend: {
       colors: {
         primary: '#1f2e13',
-        secondary: '#f4fc96',
+        secondary: 'white',
         third: '#f3fb80',
-        fourth: "#22132e"
+        fourth: "#22132e",
+        vulcan: "#f6f6f9",
+        black: {
+          200: '#333333', 
+          300: '#1a1a1a', 
+        },
+      },
+      fontFamily: {
+        squada: ["Squada One", "serif"], // Use Squada One font
+        roboto: ['Roboto', 'serif'],
+      },
+      fontWeight: {
+        thin: '100',
+        light: '300',
+        regular: '400',
+        medium: '500',
+        bold: '700',
+        xlarge: '900',
       },
     },
   },
