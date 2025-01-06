d389d28cb5b9ec515a0198dc3d1853c559e72d1a
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-06 12:10:53 +0100

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  optimizeDeps: {
+    exclude: ['three-stdlib/libs/lottie.js'], // Excludes lottie.js from optimization
   },
-  base: '/', // Ensure the base URL is correct for your deployment
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Optional: Splitting vendor chunks for better performance
+        },
+      },
+    },
+  },
+});
