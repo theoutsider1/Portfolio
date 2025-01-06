ba3948fec6764bc3a10701914d3fa4ec49839a50
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-06 12:58:30 +0100

    update objectsControls.ts

--- frontend/src/constants/Globals/objectsControls.ts
+++ frontend/src/constants/Globals/objectsControls.ts
@@ -1,96 +1,110 @@
 
 interface Control {
     value: number;
-    min: number;
-    max: number;
+    // min: number;
+    // max: number;
+}
+
+interface DeviceValues {
+  mobile:Control,
+  desktop:Control ,
 }
 // Model Control will be used to controll the model using Leva
 interface ModelControl {
-    rotationX: Control;
-    rotationY: Control;
-    rotationZ: Control;
+    rotationX: DeviceValues;
+    rotationY: DeviceValues;
+    rotationZ: DeviceValues;
 
-    positionX: Control;
-    positionY: Control;
-    positionZ: Control;
+    positionX: DeviceValues;
+    positionY: DeviceValues;
+    positionZ: DeviceValues;
 
-    scale: Control;
+    scale: DeviceValues;
 }
 
-// wand controller
 export const wandControlConfig : ModelControl = {
-    rotationX: {
-        value : 2.8,
-        min: -10,
-        max: 10,
-      },
-      rotationY: {
-        value : 0,
-        min: -10,
-        max: 10,
-      },
-      rotationZ: {
-        value : -1.4,
-        min: -10,
-        max: 10,
-      },
-      positionX: {
-        value : -1.2,
-        min: -10,
-        max: 10,
-      },
-      positionY: {
-        value : 0.3,
-        min: -10,
-        max: 10,
-      },
-      positionZ: {
-        value : -1.5,
-        min: -10,
-        max: 10,
-      },
-      scale : {
-        value: 0.02,
-        min: 0.02,
-        max: 0.02,
-    }
-}
+  rotationX: {
+    mobile:{
+              value : 0.5,
+              // min: -10,
+              // max: 10
+            },
+    desktop:{
+              value : 0.5,
+              // min: -10,
+              // max: 10
+            },
+            },
 
-// Crystall ball controller
-export const crystallBallControlConfig : ModelControl = {
-    rotationX: {
-        value : 0.6,
-        min: -10,
-        max: 10,
-      },
-      rotationY: {
-        value : -2.6,
-        min: -10,
-        max: 10,
-      },
-      rotationZ: {
-        value : 0.4,
-        min: -10,
-        max: 10,
-      },
-      positionX: {
-        value : -8,
-        min: -10,
-        max: 10,
-      },
-      positionY: {
-        value : -1.3,
-        min: -10,
-        max: 10,
-      },
-      positionZ: {
-        value : -4.5,
-        min: -10,
-        max: 10,
-      },
-      scale : {
-        value: 0.2,
-        min: 0.1,
-        max: 10,
-    }
+    rotationY:{
+      mobile: {
+              value : 0,
+              // min: -10,
+              // max: 10,
+            },
+      desktop:{
+              value : 1,
+              // min: -10,
+              // max: 10,
+            }},
+
+    rotationZ:{
+      mobile: {
+              value : 1,
+              // min: -10,
+              // max: 10,
+            },
+      desktop: {
+              value : 1,
+              // min: -10,
+              // max: 10,
+            },
+          },
+    positionX:{
+      mobile: {
+              value : 0,
+              // min: -10,
+              // max: 10,
+    },
+      desktop: {
+              value : 0,
+              // min: -10,
+              // max: 10,
+            }
+            },
+    positionY:{
+      mobile: {
+              value : 0,
+              // min: -10,
+              // max: 10,
+            },
+      desktop: {
+              value : 0,
+              // min: -10,
+              // max: 10,
+            },
+          },
+    positionZ:{
+      mobile: {
+              value : 0.5,
+              // min: -10,
+              // max: 10,
+            },
+    desktop: {
+              value : -5,
+              // min: -10,
+              // max: 10,
+            },
+          },
+    scale :{ 
+      mobile:{
+        value: 0.35,
+        // min: 0,
+        // max: 2,
+    },
+      desktop:{
+        value: 0,
+        // min: 0,
+        // max: 2,
+  }}
 }
