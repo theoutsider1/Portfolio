4f94575597360af504f0a7e87e1b03871e796424
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-08 16:39:08 +0100

    Fix navbar overlap by adding padding-top wrapper to "about" section

--- frontend/src/sections/Stack.tsx
+++ frontend/src/sections/Stack.tsx
@@ -40,7 +40,8 @@ const HumanLangue = ()=>{
 }
   
   return (
-        <section id='stack' ref={testRef} className="bg-[#0a1020] w-full flex flex-col sm:flex-row rounded-3xl mt-8 px-4 gap-4 sm:my-12 sm:bg-slate-[#0d0d12] sm:shadow-lg sm:shadow-[#0d0d12]">
+        <section id='stack' ref={testRef} className="w-full rounded-3xl mt-8 sm:pt-24  ">
+          <div className="bg-[#0a1020] rounded-3xl flex flex-col sm:flex-row px-4 gap-4 sm:bg-slate-[#0d0d12] sm:shadow-lg sm:shadow-[#0d0d12]">
             <div className="sm:w-1/2 sm:pr-12 relative">
                 <img src='/assets/spotlight5.png' className="absolute transform  -scale-x-100 -left-4 opacity-70 rounded-3xl pointer-events-none" alt="" />
                 <h2  className="squada pt-4 sm:text-right items-center text-secondary text-4xl sm:text-6xl">Stack</h2>
@@ -93,7 +94,7 @@ const HumanLangue = ()=>{
                     </Suspense>  
                     </Canvas>     
             </div>
-                
+          </div>      
         </section>
 
   );
