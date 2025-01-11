98e5a87fa77089cb96972e3d58c56df47c7603af
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-11 03:47:54 +0100

    feat: add GSAP animation for logo scaling and fading

--- frontend/src/components/CanvasLoader.tsx
+++ frontend/src/components/CanvasLoader.tsx
@@ -1,6 +1,18 @@
+import { gsap } from "gsap";
+import { useEffect } from "react";
 
 export const CanvasLoader = ()=>{
-   
+    
+    useEffect(() => {
+        const timeline = gsap.timeline();
+    
+        timeline
+          .fromTo(
+            ".logo",
+            { scale: 1, opacity: 1 }, // Initial state
+            { scale: 3, opacity: 0, duration: 2, ease: "power3.out" } // Animate to "coming forward"
+          );
+      }, []);
     return (
         <section className="w-full h-screen flex flex-col justify-center items-center text-white">
             <img src="/assets/oowl.svg" alt="Wizard-Logo" className="logo block w-1/2"/>
