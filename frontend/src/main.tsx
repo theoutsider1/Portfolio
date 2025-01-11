8e9bc00e0b22eabab3268cad8742ae542ce97b62
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-11 04:20:41 +0100

    delete suspense from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import CanvasLoader from './components/CanvasLoader.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={<CanvasLoader/>}>
     <App />
-    </Suspense>
   </StrictMode>
 )
