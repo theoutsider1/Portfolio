cb6d12ae712da3ed587f2df6b15d11fe2c806cff
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-12 16:09:09 +0100

    update scale values in wandControlConfig

--- frontend/src/constants/Globals/objectsControls.ts
+++ frontend/src/constants/Globals/objectsControls.ts
@@ -22,6 +22,90 @@ interface ModelControl {
     scale: DeviceValues;
 }
 
+// wand controller
+// export const wandControlConfig : ModelControl = {
+//     rotationX: {
+//       mobile:{
+//                 value : 2.8,
+//                 min: -10,
+//                 max: 10},
+//       desktop:{
+//                 value : 2.8,
+//                 min: -10,
+//                 max: 10},
+//               },
+
+//       rotationY:{
+//         mobile: {
+//                 value : 0,
+//                 min: -10,
+//                 max: 10,
+//               },
+//         desktop:{
+//                 value : 0,
+//                 min: -10,
+//                 max: 10,
+//               }},
+
+//       rotationZ:{
+//         mobile: {
+//                 value : -1.4,
+//                 min: -10,
+//                 max: 10,
+//               },
+//         desktop: {
+//                 value : -1.4,
+//                 min: -10,
+//                 max: 10,
+//               },
+//             },
+//       positionX:{
+//         mobile: {
+//                 value : -1.2,
+//                 min: -10,
+//                 max: 10,
+//       },
+//         desktop: {
+//                 value : -1.2,
+//                 min: -10,
+//                 max: 10,}
+//               },
+//       positionY:{
+//         mobile: {
+//                 value : 0.3,
+//                 min: -10,
+//                 max: 10,
+//               },
+//         desktop: {
+//                 value : 0.3,
+//                 min: -10,
+//                 max: 10,
+//               },
+//             },
+//       positionZ:{
+//         mobile: {
+//                 value : -1.5,
+//                 min: -10,
+//                 max: 10,
+//               },
+//       desktop: {
+//                 value : -1.5,
+//                 min: -10,
+//                 max: 10,
+//               },
+//             },
+//       scale :{ 
+//         mobile:{
+//           value: 0.02,
+//           min: 0.02,
+//           max: 0.02,
+//       },
+//         desktop:{
+//           value: 0.02,
+//           min: 0.02,
+//           max: 0.02,
+//     }}
+// }
 export const wandControlConfig : ModelControl = {
   rotationX: {
     mobile:{
@@ -98,13 +182,55 @@ export const wandControlConfig : ModelControl = {
           },
     scale :{ 
       mobile:{
-        value: 0.35,
+        value: 0.5,
         // min: 0,
         // max: 2,
     },
       desktop:{
-        value: 0,
+        value: 0.4,
         // min: 0,
         // max: 2,
   }}
 }
+// Crystall ball controller
+// export const crystallBallControlConfig : ModelControl = {
+//     rotationX: {
+//         value : 0.6,
+//         min: -10,
+//         max: 10,
+//       },
+//       rotationY: {
+//         value : -7.8,
+//         min: -10,
+//         max: 10,
+//       },
+//       rotationZ: {
+//         value : 0.4,
+//         min: -10,
+//         max: 10,
+//       },
+//       positionX: {
+//         // value : 1.9,
+//         value : 0,
+//         min: -10,
+//         max: 10,
+//       },
+//       positionY: {
+//         value : -0.4,
+//         min: -10,
+//         max: 10,
+//       },
+//       positionZ: {
+//         // value : -3,
+//         value : -2,
+//         min: -10,
+//         max: 10,
+//       },
+//       scale : {
+//         value: 0.2,
+//         min: 0.1,
+//         max: 10,
+//     }
+// }
+
+
