6efe166b9254d1d4c28edf8e622c5e48f6c907fd
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-12 14:01:12 +0100

    Fix animation overflow issue by setting container width/height to 100% and hiding scrollbars

--- frontend/src/components/CanvasLoader.tsx
+++ frontend/src/components/CanvasLoader.tsx
@@ -10,12 +10,12 @@ export const CanvasLoader = ()=>{
           .fromTo(
             ".logo",
             { scale: 1, opacity: 1 }, // Initial state
-            { scale: 3, opacity: 0, duration: 2, ease: "power3.out" } // Animate to "coming forward"
+            { scale: 3, opacity: 0, duration: 4, ease: "power3.out" } // Animate to "coming forward"
           );
       }, []);
     return (
-        <section className="w-full h-screen flex flex-col justify-center items-center text-white">
-            <img src="/assets/oowl.svg" alt="Wizard-Logo" className="logo block w-1/2"/>
+        <section className="w-full h-screen flex flex-col justify-center items-center text-white overflow-hidden">
+            <img src="/assets/oowl.svg" alt="Wizard-Logo" className="logo block"/>
         </section>
     )   
 }
