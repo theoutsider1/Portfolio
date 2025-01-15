d5ca372bbc3b2c21221c12e1b531f889594c03d6
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2025-01-15 16:41:51 +0100

    Update font size and section height

--- frontend/src/sections/Projects.tsx
+++ frontend/src/sections/Projects.tsx
@@ -9,10 +9,10 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
     const projectSectionContent = SectionContent.Project
     const gitLink = "https://github.com/theoutsider1"
     return (
-        <section id="work" className="flex my-4 sm:my-0 sm:pt-16">
+        <section id="work" className="flex items-center h-screen  my-4 sm:my-0 sm:pt-16">
             {/* Paragraph section */}
             {!isMobile ? 
-            <>
+            <div className="w-full flex">
             <div className="sm:w-1/2 ">
                     <div className="w-full flex flex-col justify-end items-end text-secondary ">
                     <div className="sm:px-12">
@@ -22,7 +22,7 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
                                 link={gitLink}
                             />
                     </div>
-                        <p className="w-full sm:mb-0 my-8 text-sm sm:text-lg font-roboto font-light opacity-70 leading-loose px-12 text-right">
+                        <p className="w-full sm:mb-0 my-8 text-base sm:text-lg font-roboto font-light opacity-70 leading-loose px-12 text-right">
                             {projectSectionContent.paragraph}
                         </p>
                     </div>
@@ -35,7 +35,7 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
                     </div>
                    
                 </div>
-            </>
+            </div>
             : 
             <div className="flex flex-col px-4">
             {/* Tittle & Button section */}
@@ -44,7 +44,7 @@ export const Projects: React.FC<ProjectProps> = ({isMobile})=> {
             </div>
             
                     <div className="w-full h-full flex flex-col justify-end items-end text-secondary ">
-                        <p className="w-full pt-8 text-sm sm:text-lg font-roboto font-light opacity-70 text-left">
+                        <p className="w-full pt-8 text-base sm:text-lg font-roboto font-light opacity-70 text-left">
                             {projectSectionContent.paragraph}
                         </p>
                     </div>
